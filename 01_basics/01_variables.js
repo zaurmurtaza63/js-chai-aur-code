12e827314e5d07bd96044419c4f459169125cebc
Murtaza <155634916+zaurmurtaza63@users.noreply.github.com>
2025-12-31 08:10:58 +0000

    conersionOperations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1121212;
+let accountEmail = "murtaza@gmail.com";
+var accountPassword = "21323";
+accountCity = "jaipur";
+let accountData;
+
+/*
+prefere not to use var keyword var accountPassword = "21323"; ,
+prefere not to use any variable without keyword  accountCity = "jaipur";,
+*/
+
+
+// console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity , accountData] );
