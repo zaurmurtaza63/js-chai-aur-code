30c6a693a6697a5ac873648bf35cc7c008e8d094
Murtaza <155634916+zaurmurtaza63@users.noreply.github.com>
2026-01-06 10:29:31 +0000

    strings in js

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,23 @@
+//  LECTURE : 11
+
+
+/*
+
+1. use of backticks
+
+*/
+
+
+
+// Backticks
+// example : There are two variables. name and father name.
+
+let fullname = "Ghulam Murtaza";
+let fatherName = ' Fateh Khan';
+
+console.log("my name is " + fullname + " and my father name is " + fatherName);   // this is bad way to concotinate .
+
+// using backtick
+console.log(`My name is ${fullname} and my father name is ${fatherName}`);  // this is the correct way to continate and use backticks.
+
+
