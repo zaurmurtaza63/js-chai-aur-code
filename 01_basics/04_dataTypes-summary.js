9ee8c4724c61312596b478b4232ac1ae428d07b1
Murtaza <155634916+zaurmurtaza63@users.noreply.github.com>
2026-01-06 09:33:58 +0000

    data types summary

---
+++ 01_basics/04_dataTypes-summary.js
@@ -0,0 +1,31 @@
+/*
+There are two types of data types in JavaScript:
+1. Primitive Data Types
+2. Non-Primitive (Reference) Data Types
+
+
+1. Primitive Data Types:
+   - Stored in stack memory
+   - Changes are made by value
+   - A copy of the value is created
+
+   Primitive data types are:
+   1. String
+   2. Number
+   3. Boolean
+   4. Null
+   5. Undefined
+   6. BigInt
+   7. Symbol
+
+
+2. Non-Primitive (Reference) Data Types:
+   - Stored in heap memory
+   - Changes are made by reference
+   - Changes affect the original value
+
+   Non-primitive data types are:
+   1. Object
+   2. Array
+   3. Function
+*/
